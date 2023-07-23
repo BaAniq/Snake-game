1cca059c25717c17877e36ca4b5af443e0205d9f
BaAniq <basia.anisimowicz@gmail.com>
2023-07-23 15:28:17 +0200

    Create segments of a snake's body

--- snake.py
+++ snake.py
@@ -1,8 +1,19 @@
 from turtle import Screen, Turtle
 screen = Screen()
-screen.setup(width=600, height= 600)
+
+screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title("Snake game")
+
+#snake.shapesize(stretch_wid=20, stretch_len=20)
+x = 0
+y = 0
+for segment in range(0,3):
+    snake = Turtle()
+    snake.shape('square')
+    snake.color('white')
+    snake.goto(x,y)
+    x = x - 20
 
 
 
