00a267dc5a77820d188632618ab0283d66daa2f9
BaAniq <basia.anisimowicz@gmail.com>
2023-07-23 18:52:51 +0200

    Making snake move

--- snake.py
+++ snake.py
@@ -1,19 +1,34 @@
 from turtle import Screen, Turtle
+import time
 screen = Screen()
 
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title("Snake game")
+screen.tracer(0)
 
-#snake.shapesize(stretch_wid=20, stretch_len=20)
 x = 0
 y = 0
-for segment in range(0,3):
-    snake = Turtle()
-    snake.shape('square')
-    snake.color('white')
-    snake.goto(x,y)
+segment_list = []
+for segments in range(0, 3):
+    segment = Turtle()
+    segment.pu()
+    segment_list.append(segment)
+    segment.shape('square')
+    segment.color('white')
+    segment.goto(x, y)
     x = x - 20
+
+game_on = True
+move = 20
+while game_on:
+    screen.update()
+    time.sleep(1)
+    for segment in segment_list:
+        segment.pu()
+        segment.forward(move)
+
+
 
 
 
