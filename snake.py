e62c219f0b5872f4a107b23125bb34153c18fdd9
BaAniq <basia.anisimowicz@gmail.com>
2023-07-29 11:28:12 +0200

    Clean the code, made a class in the file snake.py

--- snake.py
+++ snake.py
@@ -1,38 +1,28 @@
-from turtle import Screen, Turtle
-import time
-screen = Screen()
-
-screen.setup(width=600, height=600)
-screen.bgcolor('black')
-screen.title("Snake game")
-screen.tracer(0)
-
-x = 0
-y = 0
-segment_list = []
-for segments in range(0, 3):
-    segment = Turtle()
-    segment.pu()
-    segment_list.append(segment)
-    segment.shape('square')
-    segment.color('white')
-    segment.goto(x, y)
-    x = x - 20
-
-game_on = True
-move = 20
-i = 1
-while game_on:
-    screen.update()
-    time.sleep(0.1)
-
-    for seg_num in range(len(segment_list)-1, 0, -1):
-        segment_next_x = segment_list[seg_num-1].xcor()
-        segment_next_y = segment_list[seg_num-1].ycor()
-        segment_list[seg_num].goto(segment_next_x, segment_next_y)
-    segment_list[0].forward(move)
-    segment_list[0].left(90)
+from turtle import Turtle
 
 
+class Snake:
 
-screen.exitonclick()
+    def __init__(self, ):
+        self.segment_list = []
+        self.making_segments()
+
+    def making_segments(self):
+        x = 0
+        y = 0
+        for num_segment in range(0, 3):
+            segment = Turtle()
+            segment.pu()
+            segment.shape('square')
+            segment.color('white')
+            self.segment_list.append(segment)
+            segment.goto(x, y)
+            x = x - 20
+
+    def move(self):
+        movement = 20
+        for seg_num in range(len(self.segment_list) - 1, 0, -1):
+            segment_next_x = self.segment_list[seg_num - 1].xcor()
+            segment_next_y = self.segment_list[seg_num - 1].ycor()
+            self.segment_list[seg_num].goto(segment_next_x, segment_next_y)
+        self.segment_list[0].forward(movement)
