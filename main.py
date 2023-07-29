18d98cf690626db3b66823b68fe2870186064e06
BaAniq <basia.anisimowicz@gmail.com>
2023-07-29 12:36:26 +0200

    Control the snake with a keypress

--- main.py
+++ main.py
@@ -12,10 +12,16 @@ snake = Snake()
 game_on = True
 move = 20
 i = 1
+
+screen.listen()
+screen.onkey(fun=snake.move_up, key='Up')
+screen.onkey(fun=snake.move_down, key='Down')
+screen.onkey(fun=snake.move_left, key='Left')
+screen.onkey(fun=snake.move_right, key='Right')
 while game_on:
     screen.update()
     time.sleep(0.1)
-    snake.move()
+
 
 
 
--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+movement = 20
 
 
 class Snake:
@@ -20,9 +21,31 @@ class Snake:
             x = x - 20
 
     def move(self):
-        movement = 20
         for seg_num in range(len(self.segment_list) - 1, 0, -1):
+            print(seg_num)
             segment_next_x = self.segment_list[seg_num - 1].xcor()
+            print((segment_next_x))
             segment_next_y = self.segment_list[seg_num - 1].ycor()
+            print(segment_next_y)
             self.segment_list[seg_num].goto(segment_next_x, segment_next_y)
+
+    def move_up(self):
+        self.move()
+        self.segment_list[0].setheading(90)
         self.segment_list[0].forward(movement)
+
+    def move_down(self):
+        self.move()
+        self.segment_list[0].setheading(270)
+        self.segment_list[0].forward(movement)
+
+    def move_left(self):
+        self.move()
+        self.segment_list[0].setheading(180)
+        self.segment_list[0].forward(movement)
+
+    def move_right(self):
+        self.move()
+        self.segment_list[0].setheading(0)
+        self.segment_list[0].forward(movement)
+
