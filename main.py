91d92f3244251b168bb36534ee116bef6f2ff14c
BaAniq <basia.anisimowicz@gmail.com>
2023-08-03 11:49:51 +0200

    Adding scoreboard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from score import Score
 import time
 
 screen = Screen()
@@ -11,6 +12,8 @@ screen.title("Snake game")
 screen.tracer(0)
 snake = Snake()
 food = Food()
+score = Score()
+score_number = 0
 food.placement()
 game_on = True
 move = 20
@@ -21,6 +24,7 @@ screen.onkey(fun=snake.move_up, key='Up')
 screen.onkey(fun=snake.move_down, key='Down')
 screen.onkey(fun=snake.move_left, key='Left')
 screen.onkey(fun=snake.move_right, key='Right')
+
 while game_on:
     screen.update()
     time.sleep(0.1)
@@ -28,6 +32,8 @@ while game_on:
 
     if snake.head.distance(food.food) < 15:
         food.placement()
+        score_number += 1
+        score.adding_point(score_number)
     if snake.head.xcor() == 300 or snake.head.xcor() == -300 or snake.head.ycor() == 300 or snake.head.ycor() == -300:
         snake.head.home()
 
---
+++ score.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+
+class Score:
+    def __init__(self):
+        self.text = Turtle()
+        self.text.pu()
+        self.text.goto(0, 280)
+        self.text.color('red')
+        self.text.hideturtle()
+        self.score()
+
+    def score(self):
+        self.text.write('Score: 0', False, align='center', font=('Arial', 10, 'normal'))
+
+    def adding_point(self, score_num):
+        self.text.clear()
+        self.text.write(f'Score: {score_num}', False, align='center', font=('Arial', 10, 'normal'))
